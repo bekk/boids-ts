a1b8ba45028c997e8a3b76527ef8ba18e3f0b294
Sondre Bakke <sondremartin@hotmail.com>
2025-11-12 23:29:27 +0100

    🧹

--- src/predator.ts
+++ src/predator.ts
@@ -3,11 +3,16 @@ import { clamp } from "./utils/math";
 import type { Vector2 } from "./utils/vector2";
 import type { World } from "./world";
 
+/**
+ * Representerer et rovdyr i simuleringen som jakter på boids.\
+ * Hvert rovdyr velger seg ut et bytte (boid) å jakte på, og beveger seg mot det.
+ */
 export class Predator {
   position: Vector2;
   velocity: Vector2;
-  currentPrey: Boid | null = null;
-  world: World;
+  private currentPrey: Boid | null = null;
+  private world: World;
+  private turnRadius = 50;
 
   constructor(position: Vector2, velocity: Vector2, world: World) {
     this.position = position;
@@ -15,7 +20,13 @@ export class Predator {
     this.world = world;
   }
 
+  /**
+   * Oppdaterer rovdyrens posisjon og retning
+   * @param deltaTime
+   */
   update(deltaTime: number) {
+    /* Velg bytte */
+
     if (this.currentPrey === null || Math.random() < 0.01) {
       const potentialPrey =
         this.world.boids[Math.floor(Math.random() * this.world.boids.length)];
@@ -25,30 +36,7 @@ export class Predator {
     }
 
     if (this.currentPrey) {
-      const toPrey = this.currentPrey.position.sub(this.position);
-      // sjekk om man kan ta "snarveien" rundt world edges
-      if (toPrey.x > this.world.width / 2) {
-        toPrey.x -= this.world.width;
-      } else if (toPrey.x < -this.world.width / 2) {
-        toPrey.x += this.world.width;
-      }
-      if (toPrey.y > this.world.height / 2) {
-        toPrey.y -= this.world.height;
-      } else if (toPrey.y < -this.world.height / 2) {
-        toPrey.y += this.world.height;
-      }
-
-      const turnRadius = 50;
-      const angleToPrey = this.velocity.angle(toPrey);
-      const maxSpeed = this.world.parameters.value.predatorSpeed;
-      const angularVelocity = maxSpeed / turnRadius;
-      const maxTurnAngle = angularVelocity * deltaTime;
-      const clampedTurnAngle = clamp(angleToPrey, -maxTurnAngle, maxTurnAngle);
-
-      this.velocity = this.velocity
-        .rotate(clampedTurnAngle)
-        .normalize()
-        .mul(maxSpeed);
+      this.updateVelocity(this.currentPrey.position, deltaTime);
     }
 
     this.position = this.position.add(this.velocity.mul(deltaTime));
@@ -60,5 +48,35 @@ export class Predator {
     if (this.position.y < 0) this.position.y += this.world.height;
     if (this.position.y >= this.world.height)
       this.position.y -= this.world.height;
+  }
+
+  private updateVelocity(preyPosition: Vector2, deltaTime: number) {
+    const toPrey = preyPosition.sub(this.position);
+
+    // sjekk om man kan ta "snarveien" rundt world edges
+    if (toPrey.x > this.world.width / 2) {
+      toPrey.x -= this.world.width;
+    } else if (toPrey.x < -this.world.width / 2) {
+      toPrey.x += this.world.width;
+    }
+    if (toPrey.y > this.world.height / 2) {
+      toPrey.y -= this.world.height;
+    } else if (toPrey.y < -this.world.height / 2) {
+      toPrey.y += this.world.height;
+    }
+
+    // finn maksimal svingvinkel for denne framen
+    const targetSpeed = this.world.parameters.value.predatorSpeed;
+    const angularVelocity =
+      this.world.parameters.value.predatorSpeed / this.turnRadius;
+    const maxTurnAngle = angularVelocity * deltaTime;
+
+    const angleToPrey = this.velocity.angle(toPrey);
+    const clampedTurnAngle = clamp(angleToPrey, -maxTurnAngle, maxTurnAngle);
+
+    this.velocity = this.velocity
+      .rotate(clampedTurnAngle)
+      .normalize()
+      .mul(targetSpeed);
   }
 }
