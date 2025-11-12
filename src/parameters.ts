40b89096d971532987fe9e57b003455f5c4d66d1
Sondre Bakke <sondremartin@hotmail.com>
2025-11-12 19:13:13 +0100

    Du vil takke deg selv senere

--- src/parameters.ts
+++ src/parameters.ts
@@ -1,4 +1,5 @@
-interface InternalParameters {
+/** Parameterverdier for boidene */
+export interface Parameters {
   cohesionWeight: number;
   alignmentWeight: number;
   separationWeight: number;
@@ -10,9 +11,11 @@ interface InternalParameters {
   numBoids: number;
 }
 
-export type ParameterName = keyof InternalParameters;
+/** Et navn på en parameter som kan settes i {@link PersistedParameters} */
+export type ParameterName = keyof Parameters;
 
-const defaultParameters: InternalParameters = {
+/** Standardverdier som brukes når verdiene ikke finnes i localStorage */
+const defaultParameters: Parameters = {
   cohesionWeight: 1,
   alignmentWeight: 1,
   separationWeight: 1,
@@ -24,25 +27,42 @@ const defaultParameters: InternalParameters = {
   numBoids: 2000,
 };
 
-export class Parameters {
-  private _parameters: InternalParameters;
+/**
+ * En klasse som håndterer lagring og henting av boid-parametere i localStorage.
+ * Gir også mulighet for å lytte på endringer av parametere.
+ * Ved opprettelse hentes lagrede parametere fra localStorage, eller standardverdier brukes hvis ingen lagrede verdier finnes.
+ * Standardverdier kan endres i {@link defaultParameters}.
+ */
+export class PersistedParameters {
+  private _parameters: Parameters;
+  private _subscribers: {
+    [K in ParameterName]?: Array<(newValue: Parameters[K]) => void>;
+  } = {};
 
+  /** Oppretter en instans av PersistedParameters.
+   * Hver enkelt parameterverdi hentes fra localStorage hvis den finnes, ellers brukes standardverdien fra {@link defaultParameters}.
+   */
   constructor() {
     const storedValue = localStorage.getItem("boidParameters");
     if (storedValue) {
-      this._parameters = JSON.parse(storedValue);
+      // merge lagrede verdier med defaults, i tilfelle en enkeltparameter mangler
+      this._parameters = { ...defaultParameters, ...JSON.parse(storedValue) };
     } else {
       this._parameters = { ...defaultParameters };
     }
   }
 
-  private _subscribers: {
-    [K in ParameterName]?: Array<(newValue: InternalParameters[K]) => void>;
-  } = {};
-
+  /**
+   * Legger til en lytter som kalles når en parameter endres
+   * @param key Navnet på parameteren som skal lyttes på
+   * @param listener Funksjonen som kalles når parameteren endres
+   * @example parameters.onChange("numBoids", (newNumBoids) => {
+   *   console.log("Number of boids changed to", newNumBoids);
+   * });
+   */
   public onChange<K extends ParameterName>(
     key: K,
-    listener: (newValue: InternalParameters[K]) => void
+    listener: (newValue: Parameters[K]) => void
   ) {
     if (!this._subscribers[key]) {
       this._subscribers[key] = [];
@@ -50,20 +70,24 @@ export class Parameters {
     this._subscribers[key]!.push(listener);
   }
 
-  public get value(): InternalParameters {
+  /** En kopi av alle parameterverdier.\
+   * Merk at dette returnerer en kopi, så endringer på objektet vil ikke påvirke de faktiske parameterne.\
+   * For å endre en parameter, bruk {@link setParameter}
+   */
+  public get value(): Parameters {
     return { ...this._parameters };
   }
 
-  public setParameter<K extends ParameterName>(
-    key: K,
-    value: InternalParameters[K]
-  ) {
+  /**
+   * Setter verdien til en gitt parameter.\
+   * Verdien blir lagret i localStorage, og alle lyttere for denne parameteren blir kalt.
+   * @param key Navnet på parameteren som skal settes.
+   * @param value Den nye verdien for parameteren.
+   * @example parameters.setParameter("maxSpeed", 300);
+   */
+  public setParameter<K extends ParameterName>(key: K, value: Parameters[K]) {
     this._parameters[key] = value;
-    this.save();
-    this._subscribers[key]?.forEach((listener) => listener(value));
-  }
-
-  private save() {
     localStorage.setItem("boidParameters", JSON.stringify(this._parameters));
+    this._subscribers[key]?.forEach((listener) => listener(value));
   }
 }
